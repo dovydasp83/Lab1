ea53b05683f6668d6dc52ccd5b66cc01be587f69
dovydasp83 <p.dovydas135@gmail.com>
2026-09-23 19:09:57 +0300

    Add code for revert comment

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -13,6 +13,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Code for revert
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
